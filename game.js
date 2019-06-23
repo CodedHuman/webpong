a7aa1dfdc5fb11887eec2ce53e006919ae16407d
CodedHuman <sathvik1105@gmail.com>
2019-06-23 05:51:15 -0500

    Update game.js

--- game.js
+++ game.js
@@ -48,7 +48,7 @@ if (this.p1.score + 5 == this.p2.score || this.p2.score + 5 == this.p1.score) {
 window.alert("GAME OVER");
 this.p1.score = 0;
 this.p2.score = 0;
-  stopFunction();
+  stopGame();
 }
 
 // To which Y direction the paddle is moving
